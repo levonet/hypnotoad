f2a82013869f50b981b70340c5f5b54a745700ef
Pavel Bashinsky <pbashinsky@yandex-team.ru>
2014-07-25 11:46:01 +0400

    cleaning

--- .bem/enb-make.js
+++ .bem/enb-make.js
@@ -2,19 +2,12 @@ var techs = {
     levels          : require('enb/techs/levels'),
     files           : require('enb/techs/files'),
     provider        : require('enb/techs/file-provider'),
-    copy            : require('enb/techs/file-copy'),
-    merge           : require('enb/techs/file-merge'),
     bemdecl         : require('enb/techs/bemdecl-from-bemjson'),
-    modules         : require('enb-modules/techs/prepend-modules'),
-    js              : require('enb-diverse-js/techs/browser-js'),
-    roole           : require('enb-roole/techs/css-roole'),
     css             : require('enb-stylus/techs/css-stylus'),
     autoprefixer    : require('enb-autoprefixer/techs/css-autoprefixer'),
-    bemhtml         : require('enb-bemxjst/techs/bemhtml-old'),
-    bemtree         : require('enb-bemxjst/techs/bemtree-old'),
+    bemhtml         : require('enb-bemxjst/techs/bemhtml'),
     html            : require('enb/techs/html-from-bemjson'),
     borschik        : require('enb-borschik/techs/borschik'),
-    'enb-modules'   : require('enb-modules/techs/prepend-modules'),
     'deps-modules'  : require('enb-modules/techs/deps-with-modules')
 };
 
@@ -32,18 +25,12 @@ module.exports = function(config) {
             use('autoprefixer', { sourceTarget : '?.noprefix.css', destTarget : '?.css', browserSupport : getDesktopBrowsers() }),
             use('borschik', { sourceTarget: '?.css', destTarget: '?.min.css', minify: true, freeze: true }),
 
-            use('js'),
-            use('modules', { source: '?.browser.js', target: '?.js' }),
-            use('borschik', { sourceTarget: '?.js', destTarget: '?.min.js', minify: true }),
-
-            use('bemtree', { devMode: false }),
             use('bemhtml', { devMode: false }),
-            use('merge', { sources: ['?.bemtree.js', '?.bemhtml.js'], target: '?.bemxjst.js' }),
             use('html', { destTarget: '?.src.html' }),
             use('borschik', { sourceTarget: '?.src.html', destTarget: '?.html', minify: true, freeze: true }),
 
         ]);
-        nodeConfig.addTargets(['?.min.css', '?.min.js', '?.html']);
+        nodeConfig.addTargets(['?.min.css', '?.html']);
     });
 
 };
@@ -56,8 +43,8 @@ function getLevels(config) {
         { 'path':'libs/bem-components/desktop.blocks','check':false },
         { 'path':'libs/bem-components/design/common.blocks','check':false },
         { 'path':'libs/bem-components/design/desktop.blocks','check':false },
-        'common.blocks',
-        'desktop.blocks'
+        { 'path':'common.blocks','check':true },
+        { 'path':'desktop.blocks','check':true }
     ].map(function(level) { return config.resolvePath(level); });
 }
 
@@ -76,18 +63,3 @@ function getDesktopBrowsers() {
         'opera 12.16'
     ];
 }
-
-/*function getTouchPadBrowsers() {
-    return [
-        'android 4',
-        'ios 5'
-    ];
-}
-
-function getTouchPhoneBrowsers() {
-    return [
-        'android 4',
-        'ios 6',
-        'ie 10'
-    ];
-}*/
